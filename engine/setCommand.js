97b4c43ead3025c496151a6b56318640bfc6dfe3
JacquesVST <jacquesvst@gmail.com>
2022-08-03 18:36:58 -0300

    small improvements

--- engine/setCommand.js
+++ engine/setCommand.js
@@ -4,7 +4,7 @@ function setCommand(chatConfig, reddit, telegram) {
     return async () => {
         chatConfig.subs.forEach((sub) => {
             const options = { limit: sub.lastUpdate ? 100 : 1 };
-            console.log(new Date().toISOString() + ' | Fetching from ' + sub.name, options);
+            console.log(new Date().toLocaleString(), '| Fetching from', sub.name, options);
             reddit
                 .getSubreddit(sub.name)
                 .getNew(options)
@@ -16,16 +16,22 @@ function setCommand(chatConfig, reddit, telegram) {
                             if (post) {
                                 if (!sub.lastUpdate || post['created_utc'] > sub.lastUpdate) {
                                     newPosts++;
+                                    setTimeout(() => {
 
-                                    let message = `${post['subreddit_name_prefixed']}\n${post['title']}\n\nhttps://reddit.com${post['permalink']}`;
+                                        let message = `${post['subreddit_name_prefixed']}\n${post['title']}\n\nhttps://redd.it/${post['id']}`;
 
-                                    const imageUrl = post?.preview?.images[0]?.source?.url;
-                                    if (imageUrl) {
-                                        telegram.sendPhoto(chatConfig.chatId, imageUrl, { caption: message });
-                                    } else {
-                                        telegram.sendMessage(chatConfig.chatId, message);
-                                    }
+                                        const imageUrl = post?.preview?.images[0]?.source?.url;
+                                        const videoUrl = post?.media?.reddit_video?.fallback_url;
 
+                                        if (videoUrl) {
+                                            telegram.sendVideo(chatConfig.chatId, videoUrl, { caption: message });
+                                        } else if (imageUrl) {
+                                            telegram.sendPhoto(chatConfig.chatId, imageUrl, { caption: message });
+                                        } else {
+                                            telegram.sendMessage(chatConfig.chatId, message);
+                                        }
+
+                                    }, 500 * newPosts);
                                     sub.lastUpdate = post['created_utc'];
                                     updateJSON(chatConfig);
                                 }
@@ -33,10 +39,9 @@ function setCommand(chatConfig, reddit, telegram) {
                         });
 
                     console.log(
-                        `${new Date().toISOString()} | ` +
-                            (newPosts === 0
-                                ? `No recent posts from ${sub.name}`
-                                : `${newPosts}  posts from ${sub.name}`)
+                        `${new Date().toLocaleString()} |`,
+                        newPosts ? newPosts : `No recent`,
+                        `posts from ${sub.name}`
                     );
                 })
                 .catch((e) => console.error(e));
